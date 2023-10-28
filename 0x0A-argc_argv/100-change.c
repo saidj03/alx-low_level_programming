32c3778570038205874cb3de1825d751d14e0b15
saidj03 <jsaidali98@gmail.com>
2023-10-28 08:56:04 +0300

    Argument count and argument vector

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main - prints minimum number of coins to make change
+  *@argc: Agument count
+  *@argv: Argument vector
+  *Return: Always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	int coins[] = {25, 10, 5, 2, 1};
+	int coinsNumber = sizeof(coins) / sizeof(coins[0]);
+	int coinCount = 0;
+
+	for (int i = 0; i < coinsNumber; i++)
+	{
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			coinCount++;
+		}
+	}
+
+	printf("%d\n", coinCount);
+	return (0);
+}
